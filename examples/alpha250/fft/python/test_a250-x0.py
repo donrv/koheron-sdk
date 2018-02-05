fed37b4d86e2ea96a1b5dc3b3093c146aa3a4840
thomas <th.vanderbruggen@gmail.com>
2018-02-05 18:25:43 +0100

    Add A250-X0 eeprom driver

--- examples/alpha250/fft/python/test_a250-x0.py
+++ examples/alpha250/fft/python/test_a250-x0.py
@@ -17,8 +17,28 @@ class A250_X0_PowerMonitor(object):
     def get_bus_voltage(self):
         return self.client.recv_float()
 
+class A250_X0_Eeprom(object):
+    def __init__(self, client):
+        self.client = client
+    
+    @command()
+    def set_serial_number(self, sn):
+        return self.client.recv_int32()
+
+    @command()
+    def get_serial_number(self):
+        return self.client.recv_uint32()
+
 host = os.getenv('HOST', '192.168.1.17')
 client = connect(host, 'fft', restart=False)
-pwr_mon = A250_X0_PowerMonitor(client)
 
+# Power monitor
+pwr_mon = A250_X0_PowerMonitor(client)
 print("Voltage = {} V. Current = {} A".format(pwr_mon.get_bus_voltage(), 100 * pwr_mon.get_shunt_voltage()))
+
+# Eeprom
+eeprom = A250_X0_Eeprom(client)
+sn = 42
+eeprom.set_serial_number(sn)
+assert eeprom.get_serial_number() == sn
+
